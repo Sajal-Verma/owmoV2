c37ffa34921d46df302668a7f792e81241f1629a
sajal verma <mv912293@gmail.com>
2026-02-14 14:58:51 +0530

    now things is ok

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './TellWind.css'
+import './tailwind.css'
 import App from './App.jsx'
 import StoreProvider from './context/StoreProvider' // import your context provider
 
